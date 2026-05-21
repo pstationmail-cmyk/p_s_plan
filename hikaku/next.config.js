18a5b2d6d27f4f221588a4600963df7e219cd2d9
Tsuyoki <p.s.tation.mail@gmail.com>
2026-05-21 17:38:51 +0900

    Configure GitHub Pages deployment

--- hikaku/next.config.js
+++ hikaku/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  basePath: '/p_s_plan',
   output: 'export',
   trailingSlash: true,
   images: {
